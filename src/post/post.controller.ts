9bd62418ddc7c18791c4700e665f08fcb991fa59
Gabriel Duarte <gabriel.gd7@outlook.com>
2026-04-08 11:59:24 -0300

    update post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,6 +1,11 @@
 import {
 	BadRequestException,
+	Body,
 	Controller,
+	Get,
+	Param,
+	ParseUUIDPipe,
+	Patch,
 	Post,
 	Req,
 	UseGuards,
@@ -8,6 +13,8 @@ import {
 import { JwtAuthGuard } from "../auth/jwt-auth.guard";
 import { type AuthenticatedRequest } from "../auth/types/autheticated-request";
 import { CreatePostDto } from "./dto/create-post.dto";
+import { PostResponseDTO } from "./dto/post-response.dto";
+import { UpdatePostDto } from "./dto/update-post.dto";
 import { PostService } from "./post.service";
 
 @Controller("post")
@@ -16,8 +23,36 @@ export class PostController {
 
 	@UseGuards(JwtAuthGuard)
 	@Post("me")
-	async create(@Req() req: AuthenticatedRequest, dto: CreatePostDto) {
+	async create(@Req() req: AuthenticatedRequest, @Body() dto: CreatePostDto) {
 		const created = await this.postService.create(dto, req.user.id);
 		return created;
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Get("me/:id")
+	async findOneOwned(
+		@Req() req: AuthenticatedRequest,
+		@Param("id", ParseUUIDPipe) id: string,
+	) {
+		const post = await this.postService.findOneOwned({ id }, req.user);
+		return new PostResponseDTO(post);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Get("me")
+	async findAllOwned(@Req() req: AuthenticatedRequest) {
+		const posts = await this.postService.findAllOwned(req.user);
+		return posts.map((post) => new PostResponseDTO(post));
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Patch("me/:id")
+	async update(
+		@Param("id", ParseUUIDPipe) id: string,
+		@Req() req: AuthenticatedRequest,
+		@Body() dto: UpdatePostDto,
+	) {
+		const updated = await this.postService.update({ id }, dto, req.user);
+		return updated;
 	}
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,7 +1,14 @@
-import { BadRequestException, Injectable, Logger } from "@nestjs/common";
+import {
+	BadRequestException,
+	Injectable,
+	Logger,
+	NotFoundException,
+} from "@nestjs/common";
 import { PrismaService } from "../common/prisma/prisma.service";
 import { createSlugFromText } from "../common/utils/create-slug-from-text";
+import { Post, User } from "../db/prisma/client";
 import { CreatePostDto } from "./dto/create-post.dto";
+import { UpdatePostDto } from "./dto/update-post.dto";
 
 @Injectable()
 export class PostService {
@@ -28,5 +35,68 @@ export class PostService {
 				throw new BadRequestException("error during post creation");
 			});
 		return post;
+	}
+
+	async findOne(postData: Partial<Post>) {
+		const post = await this.prisma.post
+			.findFirstOrThrow({
+				where: { id: postData.id },
+				include: { author: true },
+			})
+			.catch((e) => {
+				throw new NotFoundException("Post not found");
+			});
+
+		return post;
+	}
+
+	async findOneOwned(postData: Partial<Post>, author: User) {
+		const post = await this.prisma.post
+			.findFirstOrThrow({
+				where: { id: postData.id, author: { id: author.id } },
+				include: { author: true },
+			})
+			.catch((e) => {
+				throw new NotFoundException("Post not found");
+			});
+
+		return post;
+	}
+
+	async findAllOwned(author: User) {
+		const posts = await this.prisma.post.findMany({
+			where: { author: { id: author.id } },
+			include: { author: true },
+			orderBy: { createdAt: "desc" },
+		});
+
+		return posts;
+	}
+
+	async update(postData: Partial<Post>, dto: UpdatePostDto, author: User) {
+		if (Object.keys(dto).length === 0) {
+			throw new BadRequestException("Data not sent");
+		}
+
+		const post = await this.prisma.post
+			.findFirstOrThrow({
+				where: { id: postData.id, author: { id: author.id } },
+			})
+			.catch((e) => {
+				throw new NotFoundException("Post not found");
+			});
+
+		post.title = dto.title ?? post.title;
+		post.content = dto.content ?? post.content;
+		post.excerpt = dto.excerpt ?? post.excerpt;
+		post.coverImageUrl = dto.coverImageUrl ?? post.coverImageUrl;
+		post.published = dto.published ?? post.published;
+
+		const updated = await this.prisma.post.update({
+			where: { id: post.id },
+			data: post,
+		});
+
+		return updated;
 	}
 }
